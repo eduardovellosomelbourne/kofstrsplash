158a978ec52632e953125bcc741a8a104e753427
Eduardo Velloso <eduardo.velloso@unimelb.edu.au>
2018-03-17 15:50:46 +1100

    Fix package.json

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello');
-});
+app.use(express.static('public'));
 
 app.listen(3000, function(){
    console.log('Express listening on port 3000');
 });
+
