9e97c2937b18a758e663421c094d519ad60b333e
Eduardo Velloso <eduardo.velloso@unimelb.edu.au>
2018-03-17 15:57:00 +1100

    Adjust port for heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-   console.log('Express listening on port 3000');
+app.listen(PORT, function(){
+   console.log(`Express listening on port ${PORT}`);
 });
 
